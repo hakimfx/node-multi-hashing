456288e03da1356ad80b6428ed15b0cb4a88968a
Lukman Hakim <hakimfx@gmail.com>
2017-09-22 08:08:44 +0700

    Update blake2s.c

--- blake2s.c
+++ blake2s.c
@@ -6,10 +6,12 @@
 
 #include "sha3/sph_blake2s.h"
 
-void blake2s_hash(const char* input, char* output)
+void blake2s_hash(const char* input, char* output, uint32_t len)
 {
+    uint8_t hash[BLAKE2S_OUTBYTES];
     blake2s_state ctx_blake2s;
     blake2s_init(&ctx_blake2s, BLAKE2S_OUTBYTES);
-    blake2s_update(&ctx_blake2s, input, 80);
-    blake2s_final(&ctx_blake2s, output, BLAKE2S_OUTBYTES);
+    blake2s_update(&ctx_blake2s, input, len);
+    blake2s_final(&ctx_blake2s, hash, BLAKE2S_OUTBYTES);
+    memcpy(output, hash, 32);
 }
